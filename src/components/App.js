ef3b8b2c69e3b7d88f139857d19f93e5b62cdf56
Dilshan <dilshan.mudali@gmail.com>
2021-09-24 12:41:31 -0400

    abc

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import {useState} from "react";
 import Nav from "./Nav";
-
+import MainContent from "./MainContent";
 import hogs from "../porkers_data";
+	
+	function App() {
 
-function App() {
+	const [greaseOption, setGreaseOption] = useState(false)
+
+	function showHogs() {
+		if(greaseOption === true){
+			setGreaseOption(greaseOption)
+		}else{
+			setGreaseOption(!greaseOption)
+		}
+	}
+	
 	return (
 		<div className="App">
 			<Nav />
+			<MainContent greaseOption={greaseOption} showHogs={showHogs} hogs = {hogs} />
 		</div>
 	);
 }
